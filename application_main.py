04df6d772da9e58f0b4077245cc61c226db7ec7d
Yogesh <yogesh.pathak1133@gmail.com>
2024-06-23 00:35:42 +0530

    updated log4j code

--- application_main.py
+++ application_main.py
@@ -1,6 +1,7 @@
-import sys
 from lib import DataManipulation, DataReader, Utils
 from pyspark.sql.functions import *
+
+from logger import Log4j
 
 if __name__ == '__main__':
 
@@ -11,11 +12,15 @@ if __name__ == '__main__':
 job_run_env = sys.argv[1]
 print("Creating Spark Session")
 spark = Utils.get_spark_session(job_run_env)
-print("Created Spark Session")
-orders_df = DataReader.read_orders(spark,job_run_env)
+logger = Log4j(spark)
+logger.warn('Created Spark Session')
+# print("Created Spark Session")
+orders_df = DataReader.read_orders(spark, job_run_env)
 orders_filtered = DataManipulation.filter_closed_orders(orders_df)
-customers_df = DataReader.read_customers(spark,job_run_env)
-joined_df = DataManipulation.join_orders_customers(orders_filtered,customers_df)
+customers_df = DataReader.read_customers(spark, job_run_env)
+joined_df = DataManipulation.join_orders_customers(orders_filtered, customers_df)
 aggregated_results = DataManipulation.count_orders_state(joined_df)
 aggregated_results.show()
-print("end of main")
+# print(aggregated_results.collect())
+logger.info('this is the end of main')
+# print("end of main")
--- lib/Utils.py
+++ lib/Utils.py
@@ -1,14 +1,17 @@
 from pyspark.sql import SparkSession
 from lib.ConfigReader import get_pyspark_config
 
+
 def get_spark_session(env):
     if env == "LOCAL":
         return SparkSession.builder \
-        .config(conf=get_pyspark_config(env)) \
-        .master("local[2]") \
-        .getOrCreate()
+            .config(conf=get_pyspark_config(env)) \
+            .config('spark.driver.extraJavaOptions',
+                    '-Dlog4j.configuration=file:log4j.properties') \
+            .master("local[2]") \
+            .getOrCreate()
     else:
         return SparkSession.builder \
-        .config(conf=get_pyspark_config(env)) \
-        .enableHiveSupport() \
-        .getOrCreate()
+            .config(conf=get_pyspark_config(env)) \
+            .enableHiveSupport() \
+            .getOrCreate()
